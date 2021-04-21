96c1d32a97f8c7249334a7483a4198e11fa88640
Felipe Ramos de Oliveira <felipe.roliveira@stone.com.br>
2021-04-21 15:36:48 -0300

    21/04/2021

--- Python/Building and Consuming APIs/Django Rest Framework/setup/settings.py
+++ Python/Building and Consuming APIs/Django Rest Framework/setup/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'we2q@@2p30+=^#(xbgl7e@3+r2dt+zmf#us2m$ix=q&n+=y-z$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
