a510565069ca2ce1ec90b6c016399385509d90a8
Felipe Ramos de Oliveira <felipe.roliveira@stone.com.br>
2021-06-02 09:58:16 -0300

    commit change

--- Python/Building and Consuming APIs/Django 101/escola/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from escola.models import Aluno
-
-class AlunoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Aluno
-        fields=['id','nome','rg']
